596af3992d90485ee09352fba04396cff5398fc0
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2024-12-18 13:10:48 +0100

    .

---
+++ new_datatype/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // num x = 16777215;
+    num x = 16777216;
+    printf("x: %d\n", x);
+    printf("sizeof x: %lu\n", sizeof(num));
+
+    return 0;
+
+}
