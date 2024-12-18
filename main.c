f4aa3decfef700f7ad38c2fe240bb7be6ad9dffb
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2024-12-18 11:43:14 +0100

    .

--- main.c
+++ main.c
@@ -5,32 +5,13 @@ typedef struct {
     int x;
 } S;
 
-typedef union {
-    int a;
-    int b;
-} U;
-
-
-void f(S *s) {
-    printf("s: %d\n", s.x);
-}
-
 
 int main(void) {
 
-    S s = { 45 };
+    S s = { .x = 45 };
     S *s_ptr = &s;
-    printf("s.x: %d\n", s.x);
-    printf("s_ptr.x: %d\n", s_ptr.x);
-    f(&s);
-    // s->x;
+    int x = s_ptr.x;
 
-    U u = { .a = 123 };
-    U *u_ptr = &u;
-    printf("u.a: %d\n", u.a);
-    printf("u_ptr.a: %d\n", u_ptr.a);
-    // u->a;
+    printf("x: %d\n", x);
 
-
-    return 0;
 }
